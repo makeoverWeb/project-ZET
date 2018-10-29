0aaa16cfdd9104aa790fcfe7d2860ab424579671
makeoverWeb <femakeoverweb@gmail.com>
2018-10-29 19:24:56 +0300

    add popap

--- js/script.js
+++ js/script.js
@@ -44,7 +44,20 @@ $(function() {
     }
   });
 
-   //
+   //popap
+
+  $('.show-popup').magnificPopup({
+      type: 'inline',
+      removalDelay: 500, //delay removal by X to allow out-animation
+      callbacks: {
+          beforeOpen: function () {
+              this.st.mainClass = this.st.el.attr('data-effect');
+          }
+      },
+      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
+  //
 
 
 
